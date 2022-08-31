5e51710ac91cbe0594ce46dc94ceb3a0aafa373f
Bill Wong <billwong7788@gmail.com>
2022-08-31 13:55:32 +1000

    backend packages & server init

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const PORT = 3001;
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
