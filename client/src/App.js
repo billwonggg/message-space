3d66db284eb18b71105801c2a528c3a2db2e3466
Bill Wong <billwong7788@gmail.com>
2022-08-31 23:08:25 +1000

    add socket.io and browser routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,28 @@
-import react from "react";
-import { Router, Route, Switch } from "react-router-dom";
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import ChatPage from "./pages/ChatPage";
 import { ThemeProvider } from "@mui/material";
 import { themeLight } from "./theme/theme";
+import io from "socket.io-client";
 import "./App.css";
 
+const socket = io.connect("http://localhost:3001");
+
 const App = () => {
+  const [userData, setUserData] = useState({
+    username: "",
+    room: undefined,
+  });
+  console.log(userData);
   return (
     <ThemeProvider theme={themeLight}>
-      <ChatPage />
-      {/* <RegisterPage /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/register" element={<RegisterPage setUserData={setUserData} s />} />
+          <Route exact path="/chat" element={<ChatPage />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 };
