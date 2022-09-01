4dab6aa79623f024478cf959d5ae0d06eb530f6e
Bill Wong <billwong7788@gmail.com>
2022-09-01 11:49:44 +1000

    redirect page

--- client/src/pages/ChatPage.js
+++ client/src/pages/ChatPage.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import ChatArea from "../components/ChatArea";
 import ChatNavBar from "../components/ChatNavBar";
 
 const ChatPage = (props) => {
+  const userData = props.userData;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!userData.name || !userData.room) {
+      navigate("/register");
+    }
+  }, []);
+
   return (
     <div>
       <ChatNavBar {...props} />
