da29093347c57cbd72588cbf1123349e9540e0b4
Bill Wong <billwong7788@gmail.com>
2022-09-02 17:02:48 +1000

    background theme update

--- client/src/pages/ChatPage.js
+++ client/src/pages/ChatPage.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { useTheme } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import ChatArea from "../components/ChatArea";
 import ChatNavBar from "../components/ChatNavBar";
@@ -6,6 +7,7 @@ import ChatNavBar from "../components/ChatNavBar";
 const ChatPage = (props) => {
   const userData = props.userData;
   const navigate = useNavigate();
+  const theme = useTheme();
 
   useEffect(() => {
     if (!userData.name || !userData.room) {
@@ -14,7 +16,12 @@ const ChatPage = (props) => {
   });
 
   return (
-    <div>
+    <div
+      style={{
+        minHeight: "100vh",
+        backgroundImage: theme.palette.background.bgGradient,
+      }}
+    >
       <ChatNavBar {...props} />
       <ChatArea {...props} />
     </div>
