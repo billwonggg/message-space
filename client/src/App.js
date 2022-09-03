816fdfa63040960aaa96979f5c295b496308242c
Bill Wong <billwong7788@gmail.com>
2022-09-03 10:05:28 +1000

    moved toast container to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import io from "socket.io-client";
 import RegisterPage from "./pages/RegisterPage";
 import ChatPage from "./pages/ChatPage";
 import { SelectThemeProvider } from "./theme/ThemeContext";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 import "./App.css";
 
 const socket = io.connect("http://localhost:3001");
@@ -16,6 +18,7 @@ const App = () => {
 
   return (
     <SelectThemeProvider>
+      <ToastContainer />
       <BrowserRouter>
         <Routes>
           <Route
